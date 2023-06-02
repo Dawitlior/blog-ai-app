567601724dab46884f4626998018383b34737149
Dawitlior <dawitlior777@gmail.com>
2023-06-02 17:18:55 +0300

    init

---
+++ app/(shared)/Navbar.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SocialLinks from "./SocialLinks";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <header className="mb-5">
+      <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4 ">
+        <div className="hidden sm:block">
+          <SocialLinks />
+        </div>
+        <div className="flex justify-between items-center gap-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Trending</Link>
+          <Link href="/">About</Link>
+        </div>
+        <div>
+          <p>sign in</p>
+        </div>
+      </nav>
+      <div className="flex justify-between gap-8 mt-5 mb-4 mx-10">
+        <div className="basis-2/3 md:mt-3">
+          <h1 className="font-bold text-3xl md:text-5xl">BLOG OF THE FUTURE</h1>
+          <p className="text-sm mt-3">
+            Blog dedicated towards AI and generation and job automation
+          </p>
+        </div>
+        <div className="basis-full relative w-auto h-32 bg-wh-500">
+          Image right here
+        </div>
+      </div>
+      <hr className="border-1 mx-10" />
+    </header>
+  );
+};
+export default Navbar;
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import { Open_Sans } from 'next/font/google';
+import Navbar from "app/(shared)/Navbar"
+import Footer from "app/(shared)/Footer"
+
+const openSans = Open_Sans({
+  subsets: ["latin"]
+})
+
+export const metadata = {
+  title: 'blog AI App',
+  description: 'blog built in Next JS that uses AI',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html className={openSans.className} lang="en">
+
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+    </html>
+  )
+}
