f39ccd2607347c9a4859883010eb0effb9a5740e
Dawitlior <dawitlior777@gmail.com>
2023-06-05 19:53:30 +0300

    Revert "tech-component"
    This reverts commit 3c7ef57fb680807200cdb39a9f160e32b7a30810.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Trending from "app/(home)/Trending"
-import Tech from 'app/(home)/Tech'
-import Travel from 'app/(home)/Travel'
-import Other from 'app/(shared)/Other'
+
+
 export default function Home() {
   return (
     <main className="px-10 leading-7">
      <Trending />
      <div className="md:flex gap-10 mb-5">
       <div className="basis-3/4">
-        <Tech />
-        <Travel />
-        <Other />
+        {/* <Tech /> */}
+        {/* <Travel /> */}
+        {/* <Other /> */}
         <div className="hidden md:block ">
           {/* <Subscribe /> */}
         </div>
