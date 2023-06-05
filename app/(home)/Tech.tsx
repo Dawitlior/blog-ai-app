f3e7c3cf6118b742a58d0c410accb572127ea2ec
Dawitlior <dawitlior777@gmail.com>
2023-06-05 19:55:01 +0300

    Revert "Revert "tech-component""
    This reverts commit f39ccd2607347c9a4859883010eb0effb9a5740e.

---
+++ app/(home)/Tech.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Card from '@/app/(shared)/Card'
+
+type Props = {}
+
+const Tech = (props: Props) => {
+    return (
+        <section>
+            <hr className='border-1' />
+            {/* Header */}
+            <div className='flex items-center gap-3 my-8'>
+                <h4 className='bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold'>
+                    HOT
+                </h4>
+                <p className='font-bold text-2xl'>Latest news in technologies</p>
+            </div>
+
+            {/* Flex */}
+            {/* <div className='flex justify-between items-center gap-5'>
+            <div className='bg-wh-500 h-96 basis-1/2'></div>
+            <div className='flex flex-col gap-3 h-96 basis-1/2'>
+                <div className='bg-wh-500 basis-1/3'></div>
+                <div className='bg-wh-500 basis-1/3'></div>
+                <div className='bg-wh-500 basis-1/3'></div>
+            </div>
+         </div> */}
+
+            {/* Grid */}
+            <div className='sm:grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-8 my-5'>
+                {/* Large Card */}
+                <Card className='bg-wh-500 col-span-1 row-span-3'
+                    isLongForm
+                    imageHeight='h-96'
+                />
+                {/* Small Card */}
+                <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+                    isSmallCard
+                    imageHeight='h-48'
+                />
+                <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+                    isSmallCard
+                    imageHeight='h-48'
+                />
+                <Card className='bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3'
+                    isSmallCard
+                    imageHeight='h-48'
+                />
+            </div>
+        </section>
+    )
+}
+
+export default Tech
--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -5,23 +5,23 @@ type TrendingCardProps = {
   className?: string
 };
 
-const TrendingCard = ({className}: TrendingCardProps) => {
-return (
-  <Link
-  className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
-  // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
-  href="/"
-  >
-  <div className="z-0 relative w-full h-full bg-wh-500">image</div>
-  <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
-  <div className="absolute z-2 bottom-0 left-0 p-3">
-    <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
-      category
-    </h4>
-    <div className="text-wh-100 mt-2">post title</div>
-  </div>
-  </Link>
-)
+const TrendingCard = ({ className }: TrendingCardProps) => {
+  return (
+    <Link
+      className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+      // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+      href="/"
+    >
+      <div className="z-0 relative w-full h-full bg-wh-500">image</div>
+      <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
+      <div className="absolute z-2 bottom-0 left-0 p-3">
+        <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
+          category
+        </h4>
+        <div className="text-wh-100 mt-2">post title</div>
+      </div>
+    </Link>
+  )
 }
 
 
@@ -60,6 +60,10 @@ const Trending = (props: Props) => {
         <TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
         <TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
       </div>
+
+      <p className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vel beatae impedit quia quisquam eius! Illo velit delectus voluptatibus tenetur modi similique perferendis alias amet!
+      </p>
     </section>
   );
 };
