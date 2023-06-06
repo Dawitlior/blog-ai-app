85c32dd8fbd7184acb271fcdb8dcbbe30d06b435
Dawitlior <dawitlior777@gmail.com>
2023-06-06 13:30:20 +0300

    other-component

--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './Card'
 
 type Props = {}
 
@@ -8,8 +9,11 @@ const Other = (props: Props) => {
             <hr className='border-1' />
             {/* Header */}
             <p className='font-bold text-2xl my-8'>Other trending post</p>
-            <div className=''>
-
+            <div className='sm:grid grid-cols-2 gap-16 '>
+                <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80'/>
+                <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80'/>
+                <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80'/>
+                <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80'/>
             </div>
         </section>
     )
