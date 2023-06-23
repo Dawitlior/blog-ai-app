a7e768df720c7257cc18f9a76406c3f41e527f52
Dawitlior <dawitlior777@gmail.com>
2023-06-23 03:13:49 +0300

    bring the posts to client

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,34 @@ import Travel from 'app/(home)/Travel'
 import Other from 'app/(shared)/Other'
 import Subscribe from 'app/(shared)/Subscribe'
 import Sidebar from 'app/(shared)/Sidebar'
-export default function Home() {
+import { prisma } from "app/api/client"
+import { Post } from "@prisma/client"
+
+const getPosts = async () => {
+  const posts: Array<Post> = await prisma.post.findMany();
+
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log(posts);
   return (
     <main className="px-10 leading-7">
-     <Trending />
-     <div className="md:flex gap-10 mb-5">
-      <div className="basis-3/4">
-        <Tech />
-        <Travel />
-        <Other />
-        <div className="hidden md:block ">
-          <Subscribe />
+      <Trending />
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
+          <Tech />
+          <Travel />
+          <Other />
+          <div className="hidden md:block ">
+            <Subscribe />
+          </div>
+        </div>
+        <div className="basis-1/4">
+          <Sidebar />
         </div>
       </div>
-      <div className="basis-1/4">
-        <Sidebar />
-      </div>
-     </div>
     </main>
   )
 }
