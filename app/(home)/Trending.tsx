dddf423a579e2a57e5bb7dac528c4e6a5264140b
Dawitlior <dawitlior777@gmail.com>
2023-06-23 04:38:40 +0300

    trending compo

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,33 +1,37 @@
+import { Post } from "@prisma/client";
 import Link from "next/link";
 import React from "react";
 
 type TrendingCardProps = {
-  className?: string
+  className?: string;
+  post: Post;
 };
 
-const TrendingCard = ({ className }: TrendingCardProps) => {
+const TrendingCard = ({ className, post }: TrendingCardProps) => {
   return (
     <Link
       className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
-      // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
-      href="/"
+      href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+
     >
       <div className="z-0 relative w-full h-full bg-wh-500">image</div>
       <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
       <div className="absolute z-2 bottom-0 left-0 p-3">
         <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
-          category
+          {post?.category}
         </h4>
-        <div className="text-wh-100 mt-2">post title</div>
+        <div className="text-wh-100 mt-2">{post?.title}</div>
       </div>
     </Link>
   )
 }
 
 
-type Props = {}
+type Props = {
+  trendingPosts: Array<Post>
+}
 
-const Trending = (props: Props) => {
+const Trending = ({ trendingPosts }: Props) => {
   return (
     <section className="pt-3 pb-10">
       <div className="flex items-center gap-3">
@@ -55,10 +59,10 @@ const Trending = (props: Props) => {
 
       {/* grid option */}
       <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
-        <TrendingCard className="col-span-2 row-span-2 bg-wh-500" />
-        <TrendingCard className="col-span-2 row-span-1 bg-wh-500" />
-        <TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
-        <TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
+        <TrendingCard className="col-span-2 row-span-2 bg-wh-500" post={trendingPosts[0]} />
+        <TrendingCard className="col-span-2 row-span-1 bg-wh-500" post={trendingPosts[1]} />
+        <TrendingCard className="col-span-1 row-span-1 bg-wh-500" post={trendingPosts[2]} />
+        <TrendingCard className="col-span-1 row-span-1 bg-wh-500" post={trendingPosts[3]} />
       </div>
 
       <p className="text-sm">
