5f6ea32caa2e6106a06e70ec47eedb9ea62feeee
Dawitlior <dawitlior777@gmail.com>
2023-06-23 04:38:11 +0300

    formatted post

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,48 @@
-import Trending from "app/(home)/Trending"
-import Tech from 'app/(home)/Tech'
-import Travel from 'app/(home)/Travel'
-import Other from 'app/(shared)/Other'
-import Subscribe from 'app/(shared)/Subscribe'
-import Sidebar from 'app/(shared)/Sidebar'
-import { prisma } from "app/api/client"
-import { Post } from "@prisma/client"
+import Trending from "app/(home)/Trending";
+import Tech from "app/(home)/Tech";
+import Travel from "app/(home)/Travel";
+import Other from "app/(shared)/Other";
+import Subscribe from "app/(shared)/Subscribe";
+import Sidebar from "app/(shared)/Sidebar";
+import { prisma } from "app/api/client";
+import { Post } from "@prisma/client";
 
 const getPosts = async () => {
-  const posts: Array<Post> = await prisma.post.findMany();
+  const posts = await prisma.post.findMany();
 
   return posts;
-}
+};
 
 export default async function Home() {
   const posts = await getPosts();
-  console.log(posts);
+
+  const formatPosts = () => {
+    const trendingPosts: Array<Post> = [];
+    const techPosts: Array<Post> = [];
+    const travelPosts: Array<Post> = [];
+    const otherPosts: Array<Post> = [];
+
+    posts.forEach((post: Post, i: number) => {
+      if (i < 4) {
+        trendingPosts.push(post);
+      }
+      if (post?.category === "Tech") {
+        techPosts.push(post);
+      } else if (post?.category === "Travel") {
+        travelPosts.push(post);
+      } else if (post?.category === "Interior Design") {
+        otherPosts.push(post);
+      }
+    });
+
+    return [trendingPosts, techPosts, travelPosts, otherPosts];
+  };
+
+  const [trendingPosts, techPosts, travelPosts, otherPosts] = formatPosts();
+
   return (
     <main className="px-10 leading-7">
-      <Trending />
+      <Trending trendingPosts={trendingPosts} />
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
           <Tech />
@@ -33,5 +57,5 @@ export default async function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
