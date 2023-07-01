dc9a53d8eea5a9a5321ca3dc863878deb156f2da
Dawitlior <dawitlior777@gmail.com>
2023-07-01 04:08:47 +0300

    ai modifications and logic

--- app/post/[id]/Article.tsx
+++ app/post/[id]/Article.tsx
@@ -1,13 +1,14 @@
 import { Editor, EditorContent } from "@tiptap/react";
-import React from "react";
+import React, { useState } from "react";
 import EditorMenuBar from "./editorMenuBar";
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
 
 type Props = {
-    contentError: string
+    contentError: string;
     editor: Editor | null;
-    isEditable: boolean
-    setContent: (content: string) => void
-    title: string
+    isEditable: boolean;
+    setContent: (content: string) => void;
+    title: string;
 };
 
 const Article = ({
@@ -17,11 +18,52 @@ const Article = ({
     setContent,
     title,
 }: Props) => {
+    const [role, serRole] = useState<string>("I am a helpful assistant.");
     if (!editor) {
-        return null
+        return null;
     }
+
+    const postAiContent = async () => {
+        editor
+            .chain()
+            .focus()
+            .setContent("Generating Ai Content. Please Wait...")
+            .run();
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                title: title,
+                role: role,
+            }),
+        });
+        const data = await response.json();
+
+        editor.chain().focus().setContent(data.content).run();
+        setContent(data.content);
+    };
+
     return (
         <article className="text-wh-500 leading-8">
+            {/*AI GENERATOR */}
+            {isEditable && (
+                <div className="border-2 rounded-md bg-wh-50 p-3 mb-3">
+                    <h4 className="m-0 p-0 ">Generate AI Content</h4>
+                    <p className="my-1 p-0 text-xs">What type of writer do you want?</p>
+                    <div className="flex gap-5 justify-between">
+                        <input
+                            className="border-2 rounded-md bg-wh-50 px-3 py-1 w-full"
+                            placeholder="Role"
+                            onChange={(e) => serRole(e.target.value)}
+                            value={role}
+                        />
+                        <button type="button" onClick={postAiContent}>
+                            <RocketLaunchIcon className="h-8 w-8 text-accent-orange hover:text-wh-300" />
+                        </button>
+                    </div>
+                </div>
+            )}
             <div
                 className={
                     isEditable ? "border-2 rounded-md bg-wh-50 p-3" : "w-full max-w-full "
