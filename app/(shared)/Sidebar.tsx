7133354f1fed0bf56502e1a018bcd6194599ad5c
Dawitlior <dawitlior777@gmail.com>
2023-07-19 23:32:58 +0300

    add for check

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar = (props: Props) => {
         height={1000}
       />
       <h4 className='bg-wh-900 py-3 px-5 text-wh-50 text-xs text-center font-bold'>
-        About the Blog
+        About the Blog!!!
       </h4>
       <h4 className='py-3 px-5 text-wh-500 text-center font-bold'>
        From Lior Dawit
