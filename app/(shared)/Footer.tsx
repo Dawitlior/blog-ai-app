01f8e02f29c199acd156e3bbad529e8697d79501
Dawitlior <dawitlior777@gmail.com>
2023-08-29 05:21:31 +0300

    DELETE COMMENTS

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className="bg-wh-900 text-wh-50 py-10 px-10">
       <div className="justify-between mx-auto gap-16 sm:flex">
-        {/* FIRST COLUMN */}
         <div className="mt-16 basis-1/2 sm:mt-0">
           <h4 className="font-bold">BLOG OF THE FUTURE</h4>
           <p className="my-5">
@@ -14,14 +13,12 @@ const Footer = () => {
           </p>
           <p>© Blog of the Future All Rights Reserved.</p>
         </div>
-        {/* SECOND COLUMN */}
         <div className="mt-16 basis-1/4 sm:mt-0">
           <h4 className="font-bold">Links</h4>
           <p className="my-5">Massa orci senectus</p>
           <p className="my-5">Some random link again</p>
           <p>Ullamcorper vivamus</p>
         </div>
-        {/* THIRD COLUMN */}
         <div className="mt-16 basis-1/4 sm:mt-0">
           <h4 className="font-bold">Contact Us</h4>
           <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
