674b130e3bb3b03e11b91bd08a76c1b8759b0798
Dawitlior <dawitlior777@gmail.com>
2023-08-29 05:21:38 +0300

    DELETE COMMENTS

--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -10,7 +10,6 @@ const Other = ({ otherPosts }: Props) => {
   return (
     <section className="pt-4 mb-16">
       <hr className="border-1" />
-      {/* Header */}
       <div className="flex items-center gap-3 my-2">
         <h4 className="bg-accent-green py-2 px-5 text-wh-900 text-sm font-bold">
           OTHER
